8b6dbb13d116dde894b40b4867b227fbe5df1623
michaelnordmeyer <michaelnordmeyer@users.noreply.github.com>
2018-05-05 20:58:51 +0200

    Removes accidently introduced jQuery

--- stats.js
+++ stats.js
@@ -223,6 +223,6 @@ var stats_obj = stats_obj || (function() {
 })();
 if (!window.stats_custom) var stats_custom = {};
 var stats = stats_obj.getInstance();
-$(window).on('unload', function() {
+window.onunload = function() {
     navigator.sendBeacon(stats.domain + '/?closed&uid=' + stats.get_cookie('_uuid') + '&date=' + stats.encode(new Date().toISOString()));
-});
+};
