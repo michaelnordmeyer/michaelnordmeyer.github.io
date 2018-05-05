24bcaa5688848439448da299af4df2366b0beba4
michaelnordmeyer <michaelnordmeyer@users.noreply.github.com>
2018-05-05 23:07:58 +0200

    Re-enables stats without monitoring

--- stats.js
+++ stats.js
@@ -4,7 +4,6 @@ var stats_obj = stats_obj || (function() {
   function _ins() {
     var _self = this;
     this.domain = 'https://stats.michaelnordmeyer.com';
-    console.log("Ready");
     this.set_referrer = function() {
       console.log("Setting referrer...");
       var referrer = stats_custom.iframe ? top.document.referrer : document.referrer;
@@ -22,7 +21,7 @@ var stats_obj = stats_obj || (function() {
       xhttp.open("GET", url, true);
       xhttp.send();
     };
-    this.beacon = function(type, query, called_by_pageview) {
+    this.beacon = function(type, query) {
       console.log("Firing beacon...");
       query = query || '';
       type = type || 'pageview';
@@ -53,13 +52,25 @@ var stats_obj = stats_obj || (function() {
         }
       }
       _self.store(_self.domain + '?' + type + (uid ? '=' + uid : '') + query + split + '');
-      // _self.store(_self.domain + '?' + type + (uid ? '=' + uid : '') + '&date=' + _self.encode(new Date().toISOString()) + query + split + '');
       _self.referrer = '';
+    };
+    this.pageview = function() {
+      console.log("Register pageview...");
+      _self.beacon('', '&url=' + _self.encode(_self.get_url()) + '&title=' + _self.encode(stats_custom.title || window.stats_page_title || document.title) + (_self.referrer ? '&ref=' + _self.encode(_self.referrer) : ''));
       _self.ping_start();
     };
-    this.pageview = function(only_once) {
-      console.log("Register pageview...");
-      _self.beacon('', '&url=' + _self.encode(_self.get_url()) + '&title=' + _self.encode(stats_custom.title || window.stats_page_title || document.title) + (_self.referrer ? '&ref=' + _self.encode(_self.referrer) : ''), (only_once ? 1 : 0));
+    this.ping_start = function() {
+      console.log("Starting ping...");
+      _self.ps_stop = 600;
+      var pingInterval = setInterval(_self.ping, 5 * 1000);
+      setTimeout(function() {
+        clearInterval(pingInterval);
+      }, _self.ps_stop * 1000);
+      _self.ping();
+    };
+    this.ping = function() {
+      console.log("Pinging...");
+      _self.beacon('ping');
     };
     this.get_url = function() {
       console.log("Resolving url...");
@@ -71,61 +82,8 @@ var stats_obj = stats_obj || (function() {
       if (!url) url = location.pathname + location.search;
       return url;
     };
-    this.log = function(url, title, type) {
-      console.log("Logging...");
-      if (type == 'pageview') url = url.replace(/^https?:\/\/([^\/]+)/i, '');
-      var o = {
-        'type': (type || 'click'),
-        'url': url,
-        'title': (title || '')
-      };
-      if (!_self.queue_add(o)) _self.beacon(type, o);
-    };
-    this.queue_ok = function() {
-      return window.JSON && typeof JSON == 'object' && JSON.stringify && JSON.parse;
-    };
-    this.queue_add = function(o) {
-      if (!_self.queue_ok()) return false;
-      if (o.type.match(/pageview|download|outbound/i)) return false;
-      var q = _self.queue_get();
-      try {
-        q.events.push(o);
-      } catch (e) {
-        return false;
-      }
-      _self.queue_set(q);
-      return true;
-    };
-    this.queue_reset = function() {
-      _self.queue_set(_self.queue_default());
-    };
-    this.queue_get = function() {
-      var q = _self.get_cookie('_events');
-      return q ? JSON.parse(q) : _self.queue_default();
-    };
-    this.queue_set = function(q, ex) {
-      _self.set_cookie('_events', JSON.stringify(q), (ex || 600));
-    };
-    this.queue_default = function() {
-      return {
-        'events': []
-      };
-    };
-    this.queue_process = function() {
-      var q = _self.queue_get();
-      try {
-        if (q.events.length) _self.queue_reset();
-        while (q.events.length) _self.beacon('', q.events.shift());
-      } catch (e) {
-        if (_self.debug) console.log(e);
-      }
-    };
     this.start_monitoring = function() {
       console.log("Monitoring...");
-      if (_self.queue_ok()) {
-        _self.queue_process();
-        setInterval(_self.queue_process, 5000);
-      }
       if (window.history && window.history.pushState) {
         _self.pushState = history.pushState;
         history.pushState = function() {
@@ -137,22 +95,15 @@ var stats_obj = stats_obj || (function() {
         });
       }
     };
-    this.ping_start = function() {
-      console.log("Starting ping...");
-      _self.ps_stop = 600;
-      var pingy = setInterval(_self.ping, 5 * 1000);
-      setTimeout(function() {
-        clearInterval(pingy);
-      }, _self.ps_stop * 1000);
-      _self.ping();
-    };
-    this.ping = function() {
-      console.log("Pinging...");
-      _self.beacon('ping');
+    this.add_event = function(o, type, func) {
+      if (o.addEventListener) {
+        o.addEventListener(type, func, false);
+      } else if (o.attachEvent) {
+        o.attachEvent("on" + type, func);
+      }
     };
     this.get_cookie = function(name) {
       console.log("Getting cookie " + name);
-      if (name.match(/^_(custom|referrer)/)) return '';
       var ca = document.cookie.split(';');
       for (var i = 0, l = ca.length; i < l; i++) {
         if (ca[i].match(new RegExp("\\b" + name + "="))) return decodeURIComponent(ca[i].split(name + '=')[1]);
@@ -161,12 +112,11 @@ var stats_obj = stats_obj || (function() {
     };
     this.set_cookie = function(name, value, expires) {
       console.log("Setting cookie " + name);
-      if (name.match(/^_(custom|referrer)/)) return false;
-      var ex = new Date;
+      var ex = new Date();
       ex.setTime(ex.getTime() + (expires || 20 * 365 * 86400) * 1000);
-      var temp = name + "=" + _self.encode(value) + ";expires=" + ex.toGMTString() + ";path=/;";
-      if (location.hostname.match(/\./)) temp += 'domain=.' + location.hostname.replace(/^www\./i, '') + ';';
-      document.cookie = temp;
+      var cookie = name + "=" + _self.encode(value) + ";expires=" + ex.toGMTString() + ";path=/;";
+      if (location.hostname.match(/\./)) cookie += 'domain=.' + location.hostname.replace(/^www\./i, '') + ';';
+      document.cookie = cookie;
     };
     this.uid = function() {
       var i = 0;
@@ -178,26 +128,19 @@ var stats_obj = stats_obj || (function() {
     this.encode = function(uriComponent) {
       return window.encodeodeURIComponent ? encodeURIComponent(uriComponent) : escape(uriComponent);
     };
-    this.add_event = function(o, type, func) {
-      if (o.addEventListener) {
-        o.addEventListener(type, func, false);
-      } else if (o.attachEvent) {
-        o.attachEvent("on" + type, func);
-      }
-    };
     this.ping_on_close = function() {
       navigator.sendBeacon(_self.domain + '/?end=' + _self.get_cookie('_uid'));
-      // navigator.sendBeacon(_self.domain + '/?end&' + _self.get_cookie('_uid') + '&date=' + _self.encode(new Date().toISOString()));
     };
     this.setup = function() {
       console.log("Setting up...");
+      window.addEventListener("unload", stats.ping_on_close, false);
       if (!_self.get_cookie('_referrer')) {
         _self.set_referrer();
       }
-      _self.start_monitoring();
-      _self.pageview(1);
+      // _self.start_monitoring();
+      _self.pageview();
     };
-//    _self.setup();
+   _self.setup();
   }
   return new function() {
     this.getInstance = function() {
@@ -211,4 +154,3 @@ var stats_obj = stats_obj || (function() {
 })();
 if (!window.stats_custom) var stats_custom = {};
 var stats = stats_obj.getInstance();
-window.addEventListener("unload", stats.ping_on_close, false);
