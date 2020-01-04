f5394a5040b36bd166f50def892cbab499bd0a7d
Michael Nordmeyer <michaelnordmeyer@users.noreply.github.com>
2020-01-04 13:31:06 +0100

    Reverts 'better' (after Mozilla) tablet detection

--- s.js
+++ s.js
@@ -89,7 +89,8 @@ if (isHuman(navigator.userAgent) === true) {
           return 'mobile';
         } else if (
             userAgent.includes('(iPad') ||
-            (userAgent.includes('Android') && userAgent.includes('Tablet'))) {
+            userAgent.includes('Android')) {
+//            (userAgent.includes('Android') && userAgent.includes('Tablet'))) {
           return 'tablet'
         } else if (
             userAgent.includes('(Macintosh') ||
